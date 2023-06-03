5b81efc7055ca6f05fed68fff73f09b98a01b8da
Johnny77585 <jkuo2188@gmail.com>
2023-06-03 11:20:19 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 
 const router = express.Router()
+const exerciseController = require('../controllers/exercise-controller')
+const userController = require('../controllers/user-controller')
 
-const exerciseController = require('../controllers/exercise-controller') 
-
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/exercises', exerciseController.getExercises)
-
 router.use('/', (req, res) => res.redirect('/exercises'))
 
 module.exports = router
