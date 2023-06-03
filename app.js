90351e5ccd83a465a088bfb38c8168d8de0d20e1
Johnny77585 <jkuo2188@gmail.com>
2023-06-03 18:16:59 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const express = require('express')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 const path = require('path')
-const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
+const flash = require('connect-flash')
+const session = require('express-session')
+const SESSION_SECRET = 'secret'
 
 // 使用EJS做模板
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
+  res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  next()
+})
 app.use(methodOverride('_method'))
 app.use(routes)
 
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ const express = require('express')
 const router = express.Router()
 const exerciseController = require('../controllers/exercise-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/exercises', exerciseController.getExercises)
 router.use('/', (req, res) => res.redirect('/exercises'))
-
+router.use('/', generalErrorHandler)
 module.exports = router
