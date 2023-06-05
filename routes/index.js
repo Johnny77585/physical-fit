864f68ac56a2a5dc32b2ff7f8378bbaa74e87a19
Johnny77585 <jkuo2188@gmail.com>
2023-06-05 14:54:28 +0800

    feat: passport init & signin

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 
 const router = express.Router()
+const passport = require('../config/passport')
 const exerciseController = require('../controllers/exercise-controller')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/exercises', exerciseController.getExercises)
 router.use('/', (req, res) => res.redirect('/exercises'))
 router.use('/', generalErrorHandler)
